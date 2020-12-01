395fff84c81d9b2dcd78089368fa1506c3f0f68c
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-12-01 20:37:31 +0530

    Added:Logic to check basic concept in java with sample program

--- Sample.java
+++ Sample.java
@@ -1,7 +1,19 @@
-public class Sample{
-
+public class Sample {
+  static int a=20;
   public static void main(String[] args) {
-
   System.out.println("Welcome of the Sample program being solved in the Master Branch");
+  Sample a2=new Sample("test1");
+  Sample a3=new Sample("test2",10);
+  System.out.println(Sample.a);
+  }
+  Sample(String i) {
+  System.out.println(i);
+  }
+  Sample(String k,int j) {
+  System.out.println(j);
+  System.out.println(k);
+  }
+  public void add( ) {
+  System.out.println("from method");
   }
 }
